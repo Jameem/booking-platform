ad083887d76c8dc3f1c9f1e89611a67b0e7b38c6
Jameem <jameemkurikkal.mp@gmail.com>
2020-12-04 22:07:28 +0530

    chore: server: wire up the express app

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const helmet = require("helmet")
+const rateLimit = require("express-rate-limit")
+
+require("dotenv").config()
+require("./firebase/initializeFirebase")
+var apiRouter = require("./routes/api")
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// Secure by setting http headers
+app.use(helmet())
+
+// Limit repeated requests
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+})
+app.use(limiter)
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.use("/api", apiRouter)
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
