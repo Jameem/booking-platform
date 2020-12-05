35dc72b5c5606247b975776ea66fecbf130f4b9d
Jameem <jameemkurikkal.mp@gmail.com>
2020-12-05 08:39:19 +0530

    chore: server: cleanup

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,37 @@ const router = express.Router()
 
 const orderController = require("../controllers/orderController")
 
+// @route  Get api/
+// @desc   To verify the api server is running
 router.get("/", (req, res) =>
   res.status(200).send({
     message: "Welcome to Booking Platform API v1!",
   })
 )
 
+// @route  GET api/orders
+// @desc   Get list of all orders
+// @access Public
 router.get("/orders", orderController.getOrders)
+
+// @route  GET api/orders/id
+// @desc   Get one order
+// @access Public
+router.get("/orders/:id", orderController.getOneOrder)
+
+// @route  POST api/orders
+// @desc   Create a new order
+// @access Public
 router.post("/orders", orderController.createOrder)
+
+// @route  PUT api/orders/id
+// @desc   Updates an order
+// @access Public
+router.put("/orders/:id", orderController.updateOrder)
+
+// @route  DELETE api/orders/id
+// @desc   Deletes an order
+// @access Public
+router.delete("/orders/:id", orderController.deleteOrder)
 
 module.exports = router
