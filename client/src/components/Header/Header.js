d943f5a0cfacf13aefd393a5e17095f090fdc4b9
Jameem <jameemkurikkal.mp@gmail.com>
2020-12-08 16:18:56 +0530

    chore: client: add create order link

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,18 +1,16 @@
 import React from "react"
 import "./Header.css"
 import { Link, useHistory } from "react-router-dom"
+import AddCircleIcon from "@material-ui/icons/AddCircle"
 import AccountCircleIcon from "@material-ui/icons/AccountCircle"
 import { useStateValue } from "../../StateProvider"
 import { auth } from "../../firebase"
 
 function Header() {
-  const history = useHistory()
-  const [{ basket, user }] = useStateValue()
+  const [{ user }, dispatch] = useStateValue()
 
-  const login = () => {
-    if (user) {
-      auth.signOut()
-    }
+  const signOut = () => {
+    if (user) auth.signOut()
   }
 
   return (
@@ -22,13 +20,23 @@ function Header() {
       </Link>
 
       <div className="header__nav">
-        <AccountCircleIcon />
-        <Link to="/login" className="header__link">
-          <div onClick={login} className="header__option">
-            <span className="header__optionLineOne">{user?.email}</span>
-            <span className="header__optionLineTwo">Sign Out</span>
-          </div>
-        </Link>
+        <div className="header__navItem">
+          <AddCircleIcon />
+          <Link to="/order/create" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineTwo">Create an Order</span>
+            </div>
+          </Link>
+        </div>
+        <div className="header__navItem">
+          <AccountCircleIcon />
+          <Link to="/login" className="header__link">
+            <div onClick={signOut} className="header__option">
+              <span className="header__optionLineOne">{user?.email}</span>
+              <span className="header__optionLineTwo">Sign Out</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </nav>
   )
