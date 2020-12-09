b30b0a64b96afb35178fb7bdef9488d5a76a9d27
Jameem <jameemkurikkal.mp@gmail.com>
2020-12-09 11:22:43 +0530

    chore: server: set up the static folder for production

--- app.js
+++ app.js
@@ -27,6 +27,17 @@ app.use(morgan("dev"))
 
 app.use("/api", apiRouter)
 
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  console.log("We are in Production environment...")
+  // set a static folder
+  app.use(express.static("client/build"))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
