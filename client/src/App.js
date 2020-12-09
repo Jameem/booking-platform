aad4c06e2a4f32628f9f5841a9cc935c27e0c8d6
Jameem <jameemkurikkal.mp@gmail.com>
2020-12-09 09:36:03 +0530

    chore: client: wired up toast provider and new routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom"
+import { ToastProvider } from "react-toast-notifications"
 
 import "./App.css"
 import Login from "./components/Login/Login"
 import Header from "./components/Header/Header"
 import { useStateValue } from "./StateProvider"
 import { auth } from "./firebase"
-import Order from "./components/Order/Order"
+import OrderList from "./components/Order/OrderList"
+import OrderForm from "./components/Order/OrderForm"
 
 function App() {
   const [{ user }, dispatch] = useStateValue()
+  const token = localStorage.getItem("token")
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
@@ -33,17 +36,46 @@ function App() {
   }, [])
 
   return (
-    <Router>
-      <div className="app">
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <Header />
-          <Order />
-        </Route>
-      </div>
-    </Router>
+    <ToastProvider>
+      <Router>
+        <div className="app">
+          {!token && window.location.pathname != "/login" && (
+            <Redirect to="/login" />
+          )}
+
+          {/* Login route */}
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          {/* Root route listing all the orders*/}
+          <Route path="/" exact>
+            {/* Header for the content pages*/}
+            <Header />
+            <OrderList />
+          </Route>
+
+          {/* Route for editing an order*/}
+          <Route
+            path="/order/edit/:id"
+            exact
+            render={(props) => <OrderForm {...props} mode="edit" />}
+          ></Route>
+
+          {/* Route for viewing an order*/}
+          <Route
+            path="/order/view/:id"
+            exact
+            render={(props) => <OrderForm {...props} mode="view" />}
+          ></Route>
+
+          {/* Route for creating an order*/}
+          <Route path="/order/create" exact>
+            <OrderForm mode="create" />
+          </Route>
+        </div>
+      </Router>
+    </ToastProvider>
   )
 }
 
